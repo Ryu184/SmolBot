6bba851683ef76d655af39666760997309655520
Kanakon <>
2020-08-30 19:22:03 +0200

    Rebranding to SmolBot

--- stdcommands/info/info.go
+++ stdcommands/info/info.go
@@ -14,10 +14,10 @@ var Command = &commands.YAGCommand{
 	Description: "Responds with bot information",
 	RunInDM:     true,
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		info := fmt.Sprintf(`**YAGPDB - Yet Another General Purpose Discord Bot**
+		info := fmt.Sprintf(`**SmolBot - YAGPDB - Yet Another General Purpose Discord Bot**
 This bot focuses on being configurable and therefore is one of the more advanced bots.
 It can perform a range of general purpose functionality (Reddit feeds, various commands, moderation utilities, automoderator functionality and so on) and it's configured through a web control panel.
-I'm currently being run and developed by Jonas747#0001 (105487308693757952) but the bot is open source (<https://github.com/jonas747/yagpdb>), so if you know Go and want to make some contributions, feel free to make a PR.
+I'm currently being run and developed by Kana#1111 and Jonas747#0001 but the bot is open source (<https://github.com/Kanakonn/SmolBot>), so if you know Go and want to make some contributions, feel free to make a PR.
 Control panel: <https://%s/manage>
 				`, common.ConfHost.GetString())
 
