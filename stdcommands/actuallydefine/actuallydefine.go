536dff19c0f2a80b20839c3e2b24d720fd1b4586
Kanakon <>
2020-09-03 23:21:14 +0200

    Put owlbot api key in env var, fix small bugs, replace HTML formatting with discord formatting

--- stdcommands/actuallydefine/actuallydefine.go
+++ stdcommands/actuallydefine/actuallydefine.go
@@ -3,11 +3,15 @@ package actuallydefine
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"time"
+	"strings"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+
+	"github.com/jonas747/dcmd"
+	"github.com/jonas747/yagpdb/commands"
+	"github.com/jonas747/yagpdb/common/config"
 )
 
 type SearchResult struct {
@@ -26,26 +30,28 @@ type Result struct {
 
 const API_URL = "https://owlbot.info/api/v4/dictionary/"
 
-const API_KEY = ""
+var (
+	confApiKey  = config.RegisterOption("yagpdb.owlbotapikey", "OwlBot API key", "")
+)
 
 func Query(searchTerm string) (*SearchResult, error) {
 	req, err := http.NewRequest("GET", API_URL + url.QueryEscape(searchTerm), nil)
 	if err != nil {
 		return nil, err
 	}
-  
-  req.Header.Set("Authorization", "Token " + API_KEY) 
-	
-  client := &http.Client{Timeout: time.Second * 10}
 
-  
-  resp, err := client.Do(req)
+	req.Header.Set("Authorization", "Token " + confApiKey.GetString()) 
+	
+	client := &http.Client{Timeout: time.Second * 10}
+
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
-  
-  defer resp.Body.Close()
-  
+
+	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("HTTP Response was not a 200: %d", resp.StatusCode)
 	}
@@ -87,12 +93,19 @@ var Command = &commands.YAGCommand{
 		word := qResp.Word
 		pronunciation := qResp.Pronunciation
 
-		cmdResp := fmt.Sprintf("**%s**[%s]:\n 1. %s: %s\n*%s\n*", word, pronunciation, result[0].Type, result[0].Definition, result[0].Example)
-    if len(qResp.Results) > 1 {
+		cmdResp := fmt.Sprintf("**%s**[%s]:\n 1. %s: %s\n*%s*\n", word, pronunciation, result[0].Type, result[0].Definition, result[0].Example)
+
+		if len(qResp.Results) > 1 {
 			for i := 1 ; i < len(qResp.Results) ; i++{
 				cmdResp += fmt.Sprintf("%d. %s: %s\n%s\n", i+1, result[i].Type, result[i].Definition, result[i].Example)		
-        }
+			}
 		}
+		
+		cmdResp = strings.ReplaceAll(cmdResp, "<b>", "**")
+		cmdResp = strings.ReplaceAll(cmdResp, "</b>", "**")
+		cmdResp = strings.ReplaceAll(cmdResp, "<i>", "*")
+		cmdResp = strings.ReplaceAll(cmdResp, "</i>", "*")
+
 		return cmdResp, nil
 	},
 }
